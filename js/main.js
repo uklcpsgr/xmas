654b38b32251b4effb86ef1a004f41e39612e7eb
Oleg Vlakhno <oleg.vlakhno@gmail.com>
2015-12-20 14:08:59 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  "use strict";
+
+  var initSlide = function() {
+    var toyWraps = document.getElementsByClassName('toy-wrap');
+    // Center slide index should be used.
+    $(toyWraps[1]).removeClass('init');
+  };
+
+  var owl = $(".owl-carousel").owlCarousel({
+    items: 1,
+    startPosition: 1,
+    URLhashListener: true,
+//    onInitialized: initSlide,
+  });
+  
+  owl.on('changed.owl.carousel', function(event) {
+    var toyWraps = document.getElementsByClassName('toy-wrap');
+    $(toyWraps).each(function() {
+      $(this).addClass('init');
+    });
+    $(toyWraps[event.item.index]).removeClass('init');
+  });
+
+
+  var xMasSong = document.getElementById('bg-song');
+  xMasSong.addEventListener("canplay", function(e) {
+    e.target.play();
+    initSlide();
+    $(".snowman").fadeOut();
+  });
+});
